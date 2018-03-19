5b9b5617f2082f3bfd2fe7f2248e6cf505815cdf
Matthew Fritz <matthew.fritz@csun.edu>
2018-03-19 09:51:13 -0700

    Added DELETE route with an optional parameter constrained by a regex

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,10 @@ $router->group(['prefix' => '1.0'], function () use ($router) {
     	'as' => 'citations.index',
     	'uses' => 'CitationsController@index'
     ]);
+    // this DELETE route handles both the single deletion case as well as
+    // the case where an email will be provided as part of the query string
+    $router->delete('citations[/{id:[0-9]+}]', [
+        'as' => 'citations.delete',
+        'uses' => 'CitationsController@delete'
+    ]);
 });
