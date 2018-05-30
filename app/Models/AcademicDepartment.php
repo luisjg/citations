e6ebe4852819b74bc1a11b33823ad40783dbc58d
Matthew Fritz <matthew.fritz@csun.edu>
2018-05-30 13:22:16 -0700

    Implement a model mapping between an academic group and its set of child departments

---
+++ app/Models/AcademicDepartment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AcademicDepartment extends Model
+{
+	protected $table = "nemo.academicDepartments";
+	protected $primaryKey = "entities_id";
+	public $incrementing = false;
+
+	/**
+	 * Query scope to filter records by the department ID and without having
+	 * to prepend the "academic_departments:" collection manually.
+	 *
+	 * @param int $id The ID of the department
+	 * @return Builder
+	 */
+	public function scopeWhereDepartmentId($query, $id) {
+		return $query->where("entities_id", "academic_departments:{$id}");
+	}
+}
