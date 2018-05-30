17cea3fc2a5ce7e7e8a5169fe54ee076a0b6362f
Matthew Fritz <matthew.fritz@csun.edu>
2018-05-30 14:28:39 -0700

    Update to the About page

--- resources/views/pages/about/version-history.blade.php
+++ resources/views/pages/about/version-history.blade.php
@@ -6,6 +6,17 @@
 
 @section('content')
   <h1 id="introduction" class="type--header type--thin">Version History</h1>
+  <h2>{{ env('APP_NAME') }} 1.1.0 <small>Release Date: TBD</small></h2>
+  <p>
+    Additional functionality for retrieval of groups of information
+    <br /><br />
+    <strong>New Features:</strong>
+    <ol>
+      <li>Ability to retrieve all citations for a given department.</li>
+      <li>Ability to retrieve all citations for a given college.</li>
+      <li>Ability to limit the citations to the most recent X entries with a filter.</li>
+    </ol>
+  </p>
   <h2>{{ env('APP_NAME') }} 1.0.0 <small>Release Date: 03/29/18</small></h2>
   <p>
     Initial Release
