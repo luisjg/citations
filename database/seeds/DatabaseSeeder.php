3570939f8eaf7616f812a60e6b06b6353812e84e
Matthew Fritz <matthew.fritz@csun.edu>
2018-06-08 13:09:01 -0700

    Forgot to un-comment the API key seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call('PermissionScopeTableSeeder');
 
         // un-comment this to generate three API keys
-        $this->call('KeysTableSeeder');
+        //$this->call('KeysTableSeeder');
     }
 }
