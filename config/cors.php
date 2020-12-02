ca1bcbe8d921ba17fa13ff3db4c432be9c5c3bf6
luisjg <luis.guzman@csun.edu>
2020-12-02 13:29:33 -0800

    Adjust the cors package settings.

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
      * You can enable CORS for 1 or multiple paths.
      * Example: ['api/*']
      */
-    'paths' => ['api/*'],
+    'paths' => ['1.0/*', '1.1/*'],
 
     /*
     * Matches the request method. `['*']` allows all methods.
