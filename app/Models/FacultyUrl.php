fe55a7b1fc49ad5f51690c6c0f54256e14a0fe8a
luisjg <luis.guzman@csun.edu>
2021-03-22 15:29:55 -0700

    Remove private information.

--- app/Models/FacultyUrl.php
+++ app/Models/FacultyUrl.php
@@ -9,4 +9,7 @@ class FacultyUrl extends Model
 	protected $table = "bedrock.faculty_urls";
 	protected $primaryKey = "user_id";
 	public $incrementing = false;
+	protected $hidden = [
+	    'user_id'
+    ];
 }
